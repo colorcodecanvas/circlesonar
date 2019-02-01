95848c772d5548a6a6dc9a851f1250bb0bc7a1e5
Sanjeev Giri <sgiri@vertexinc.com>
2019-02-01 09:35:09 -0500

    failed test

--- src/main/java/com/colorcodecanvas/circlesonar/color/ColorService.java
+++ src/main/java/com/colorcodecanvas/circlesonar/color/ColorService.java
@@ -1,7 +1,17 @@
 package com.colorcodecanvas.circlesonar.color;
 
+import static io.vavr.API.$;
+import static io.vavr.API.Case;
+import static io.vavr.API.Match;
+
 public class ColorService {
   public String guessColorByAnimal(String animal) {
-    return animal;
+    return Match(animal).of(
+      Case($("lion"), "yellow"),
+      Case($("dog"), "black"),
+      Case($("cat"), "blue"),
+      Case($("elephant"), "grey"),
+      Case($(), "white")
+    );
   }
 }
---
+++ src/test/groovy/com/colorcodecanvas/circlesonar/color/ColorServiceSpec.groovy
@@ -0,0 +1,15 @@
+package com.colorcodecanvas.circlesonar.color
+
+import spock.lang.Specification
+
+class ColorServiceSpec extends Specification {
+  ColorService service = new ColorService()
+
+  def "guess color for a person with lion as favorite animal"() {
+    when:
+    def guessedColor = service.guessColorByAnimal('lion')
+
+    then:
+    guessedColor == 'red'
+  }
+}
