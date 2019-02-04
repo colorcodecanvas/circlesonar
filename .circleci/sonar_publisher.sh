2c69cbf5f04f1f524396239a774acefb1a006f4a
Sanjeev Giri <sgiri@vertexinc.com>
2019-02-04 13:31:24 -0500

    when to merge

--- .circleci/sonar_publisher.sh
+++ .circleci/sonar_publisher.sh
@@ -7,6 +7,10 @@ if [ "$CIRCLE_BRANCH" == "master" ]; then
     -Dsonar.host.url=https://sonarcloud.io \
     -Dsonar.login=218a21bec532782bc07312d4ff869ea329e21171
 else
+  echo circle ci master commit head bug fix defined in https://community.sonarsource.com/t/code-is-empty-on-pull-request-reviews/822
+  git fetch --all
+  git branch -D master
+  git rev-parse origin/master
   mvn sonar:sonar \
     -Dsonar.projectKey=colorcodecanvas_circlesonar \
     -Dsonar.organization=colorcodecanvas \
