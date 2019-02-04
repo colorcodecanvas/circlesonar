bc6347243ee091d05c5f1239a79f10aac671ac86
Sanjeev Giri <sgiri@vertexinc.com>
2019-02-04 13:26:23 -0500

    update script

--- .circleci/sonar_publisher.sh
+++ .circleci/sonar_publisher.sh
@@ -5,7 +5,7 @@ if [ "$CIRCLE_BRANCH" == "master" ]; then
     -Dsonar.projectKey=colorcodecanvas_circlesonar \
     -Dsonar.organization=colorcodecanvas \
     -Dsonar.host.url=https://sonarcloud.io \
-    -Dsonar.login=218a21bec532782bc07312d4ff869ea329e21171 \
+    -Dsonar.login=218a21bec532782bc07312d4ff869ea329e21171
 else
   mvn sonar:sonar \
     -Dsonar.projectKey=colorcodecanvas_circlesonar \
@@ -17,5 +17,5 @@ else
     -Dsonar.pullrequest.branch="$CIRCLE_BRANCH" \
     -Dsonar.pullrequest.provider=github \
     -Dsonar.pullrequest.github.repository="$CIRCLE_PROJECT_REPONAME" \
-    -Dsonar.pullrequest.github.endpoint=https://api.github.com/ \
+    -Dsonar.pullrequest.github.endpoint=https://api.github.com/
 fi
