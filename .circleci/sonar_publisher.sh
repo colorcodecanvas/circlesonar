a8a9370e2b1426750594c89a0314f4253ff1a309
Sanjeev Giri <sgiri@vertexinc.com>
2019-02-04 13:28:53 -0500

    SG-000: Sonar publish results (#3)
    * failed test
    
    * include plugins
    
    * updated java version
    
    * publish results to sonar
    
    * import
    
    * hardcoded pr id and analysis
    
    * force import failure
    
    * force formatting failure
    
    * config
    
    * config
    
    * remove publish part
    
    * publish to sonar
    
    * remove store artifacts
    
    * image
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * script updated
    
    * ci script updated with dynamic pr id
    
    * github pr decoration
    
    * fix script
    
    * config for master
    
    * config for master
    
    * sonar publisher
    
    * pwd
    
    * pwd
    
    * pwd
    
    * update file mode
    
    * script updated
    
    * bash
    
    * updated line endings
    
    * update script

---
+++ .circleci/sonar_publisher.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+if [ "$CIRCLE_BRANCH" == "master" ]; then
+  mvn sonar:sonar \
+    -Dsonar.projectKey=colorcodecanvas_circlesonar \
+    -Dsonar.organization=colorcodecanvas \
+    -Dsonar.host.url=https://sonarcloud.io \
+    -Dsonar.login=218a21bec532782bc07312d4ff869ea329e21171
+else
+  mvn sonar:sonar \
+    -Dsonar.projectKey=colorcodecanvas_circlesonar \
+    -Dsonar.organization=colorcodecanvas \
+    -Dsonar.host.url=https://sonarcloud.io \
+    -Dsonar.login=218a21bec532782bc07312d4ff869ea329e21171 \
+    -Dsonar.pullrequest.base=master \
+    -Dsonar.pullrequest.key=${CI_PULL_REQUEST##*/} \
+    -Dsonar.pullrequest.branch="$CIRCLE_BRANCH" \
+    -Dsonar.pullrequest.provider=github \
+    -Dsonar.pullrequest.github.repository="$CIRCLE_PROJECT_REPONAME" \
+    -Dsonar.pullrequest.github.endpoint=https://api.github.com/
+fi
--- src/main/java/com/colorcodecanvas/circlesonar/color/ColorService.java
+++ src/main/java/com/colorcodecanvas/circlesonar/color/ColorService.java
@@ -1,7 +1,25 @@
 package com.colorcodecanvas.circlesonar.color;
 
+
+import java.util.Optional;
+import java.util.Date;
+
 public class ColorService {
   public String guessColorByAnimal(String animal) {
-    return animal;
+    String color;
+    switch (animal) {
+          case "lion":
+        color = "yellow";
+        break;
+      case "dog":
+        color = "black";
+        break;
+      case "cat":
+        color = "blue";
+        break;
+      default:
+        color = "white";
+    }
+    return color;
   }
 }
